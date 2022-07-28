7060f2415a69db3538a29e25143cb34f175e4004
Ankitkumar <56481469+Ankitkumar35@users.noreply.github.com>
2022-07-28 23:00:34 +0530

    final commit

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const connectDB = require('./config/db');
 
 // const router = require('./routes/api/users');
 
+const path = require('path');
+
 const app = express();
 
 connectDB();
@@ -20,6 +22,16 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
